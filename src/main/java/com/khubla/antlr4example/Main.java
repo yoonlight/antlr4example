34d2d893a05ef4c4c343edfd407c6b8289a0c644
yoonlight <yoonlight12@gmail.com>
2022-05-26 01:16:09 +0900

    feat: add cli library picocli

--- src/main/java/com/khubla/antlr4example/Main.java
+++ src/main/java/com/khubla/antlr4example/Main.java
@@ -5,6 +5,10 @@ import java.io.InputStream;
 
 import com.khubla.antlr4example.ast.HTML;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -12,17 +16,22 @@ import java.io.FileWriter;
 /**
  * @author Tom Everett
  */
-class Main {
+@Command
+class Main implements Runnable {
+   @Option(names = { "-i", "--input" })
+   private static String inputFile = "example.asp";
+   @Option(names = { "-o", "--output" })
+   private static String outputFile = "asp.json";
+   @Option(names = { "-p", "--print" })
+   private static boolean printString;
+   @Option(names = { "-w", "--write" })
+   private static boolean writeString;
 
    public static void main(String[] args) {
-      try {
-         String inputFile = "example.asp";
-         String outputFile = "asp.json";
+      CommandLine.run(new Main(), args);
 
-         if (args.length == 2) {
-            inputFile = args[0];
-            outputFile = args[1];
-         }
+      try {
+
          /*
           * get the input file as an InputStream
           */
@@ -30,15 +39,26 @@ class Main {
 
          InputStream inputStream = new FileInputStream(readFile);
          String result = HTML.parse(inputStream);
-
-         System.out.println(result);
-
-         try (FileWriter file = new FileWriter(outputFile)) {
-            file.write(result);
-            file.flush();
+         if (printString) {
+            System.out.println(result);
          }
+         if (writeString) {
+
+            try (FileWriter file = new FileWriter(outputFile)) {
+               file.write(result);
+               file.flush();
+            }
+         }
+
       } catch (IOException e) {
          e.printStackTrace();
       }
    }
+
+   @Override
+   public void run() {
+
+      System.out.println("ANTLR Command Line Interface");
+   }
+
 }
